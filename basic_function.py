4f414cffe87e63b9f122a449d879020136309673
James Douglass <jamesdouglassusa@gmail.com>
2021-02-21 15:09:26 -0800

    Basic test function.

---
+++ basic_function.py
@@ -0,0 +1,13 @@
+def foo(a, b):
+    """Test test
+
+    Args:
+        a (int): Nope a
+            proper indent
+                improper indent
+        b (int): Nope b
+
+    Return:
+        1
+    """
+    return 1
